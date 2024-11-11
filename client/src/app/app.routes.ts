f80feef191e8ebb9e4e8bb999ac7877d795f35eb
Georgeking <visualbasic321@gmail.com>
2024-11-10 20:56:46 -0600

    completed activity-9

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+import {CreatePostsComponent} from './create-posts/creata-post.Component';
+import {ViewPostCompontent} from '.view-post/view-post.component';
+
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'create', component: CreatePostsComponent},
+  {path: 'posts/:id', component: ViewPostCompontent},
+  { path: '**', component:HomeComponent,pathMatch:'full'},
+
+
+];
